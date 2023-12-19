9b5b5093088b46b345834fd16ce07087755c9914
RybiiVladyslav <ribij.vlad.ik@gmail.com>
2023-12-19 19:58:06 +0200

    Added property CountOrders and method for works with it

--- Po.Core/Pizza.cs
+++ Po.Core/Pizza.cs
@@ -4,11 +4,23 @@
     {
         public string Type { get; set; }
         public List<string> Ingredients { get; set; }
+        public int CountOrders { get; private set; }
 
         public Pizza(string type, List<string> ingredients)
         {
             Type = type;
             Ingredients = ingredients;
+            CountOrders = 0;
+        }
+
+        public void AddOrder()
+        {
+            CountOrders++;
+        }
+
+        public void RemoveOrder()
+        {
+            CountOrders--;
         }
     }
 }
